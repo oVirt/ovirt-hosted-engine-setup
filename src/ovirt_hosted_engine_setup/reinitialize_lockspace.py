1684e54b1d2bd775c3c969044d085f517706155d
Martin Sivak <msivak@redhat.com>
2015-06-09 15:47:41 +0200

    Add two new recover commands to the hosted-engine tool
    This adds new interface to two hosted engine operations:
    
    - sanlock lockspace reinitialization
    - host metadata cleanup
    
    Change-Id: I7cdb65057de3cf75a1b2919030a8ec79523b36d3
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1116469
    Signed-off-by: Martin Sivak <msivak@redhat.com>

---
+++ src/ovirt_hosted_engine_setup/reinitialize_lockspace.py
@@ -0,0 +1,30 @@
+#
+# ovirt-hosted-engine-setup -- ovirt hosted engine setup
+# Copyright (C) 2013-2015 Red Hat, Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+
+
+"""Reinitialize sanlock lockspace"""
+
+import sys
+from ovirt_hosted_engine_ha.client import client
+
+
+if __name__ == "__main__":
+    force = len(sys.argv) > 1 and sys.argv[1] == "--force"
+    ha_cli = client.HAClient()
+    ha_cli.reset_lockspace(force)
