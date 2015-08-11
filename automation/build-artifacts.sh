552e3264804d63cf57e55617b2c94a11e9964c65
Sandro Bonazzola <sbonazzo@redhat.com>
2015-08-11 09:02:22 +0200

    automation: align to common build code
    - add git hash and timestamp to the rpm
    
    Change-Id: I1934deff52efae9f7cf45970f4218820d974bdb9
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -1,12 +1,24 @@
-#!/bin/bash -e
-rm -rf exported-artifacts
-mkdir exported-artifacts
+#!/bin/bash -xe
+[[ -d exported-artifacts ]] \
+|| mkdir -p exported-artifacts
+
+[[ -d tmp.repos ]] \
+|| mkdir -p tmp.repos
+
+SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
+
 autoreconf -ivf
 ./configure
 make dist
-mv *.tar.gz exported-artifacts
 yum-builddep ovirt-hosted-engine-setup.spec
 rpmbuild \
-    -D "_srcrpmdir $PWD/exported-artifacts"  \
-    -D "_rpmdir $PWD/exported-artifacts"  \
-    -ta exported-artifacts/*.gz
+    -D "_srcrpmdir $PWD/tmp.repos" \
+    -D "_topmdir $PWD/tmp.repos" \
+    -D "release_suffix ${SUFFIX}" \
+    -ta ovirt-hosted-engine-setup-*.tar.gz
+
+mv *.tar.gz exported-artifacts
+find \
+    "$PWD/tmp.repos" \
+    -iname \*.rpm \
+    -exec mv {} exported-artifacts/ \;
