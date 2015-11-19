e5b1ddc7f4d9814dc905f91f1c94e29eaa27d93e
Simone Tiraboschi <stirabos@redhat.com>
2015-11-19 15:38:25 +0100

    packaging: setup: add CLI disconnect-storage option
    Add disconnect-storage CLI option
    
    Change-Id: Iffa3fc9ad1cfdec356ae8762c3bbd1480c91905b
    Depends-On: https://gerrit.ovirt.org/69931
    Bug-Url: https://bugzilla.redhat.com/1001181
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>
    (cherry picked from commit ec0440ed5a0eba71415827e39408307e619a8dd9)

---
+++ src/ovirt_hosted_engine_setup/disconnect_storage_server.py
@@ -0,0 +1,28 @@
+#
+# ovirt-hosted-engine-setup -- ovirt hosted engine setup
+# Copyright (C) 2017 Red Hat, Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+
+
+"""Disconnect storage server"""
+
+from ovirt_hosted_engine_ha.client import client
+
+
+if __name__ == "__main__":
+    ha_cli = client.HAClient()
+    ha_cli.disconnect_storage_server()
