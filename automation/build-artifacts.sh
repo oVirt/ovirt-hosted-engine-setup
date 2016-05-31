384ac1bbef3b5d5500e4b59072e36e9a1c93e84f
Sandro Bonazzola <sbonazzo@redhat.com>
2016-05-31 10:58:42 +0200

    build: ovirt-hosted-engine-setup-2.0.0
    Change-Id: I4a0885d65ae94ce2d0c30997e7859a3349b7d7a3
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -5,7 +5,6 @@
 [[ -d tmp.repos ]] \
 || mkdir -p tmp.repos
 
-SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
 
 autoreconf -ivf
 ./configure
@@ -13,7 +12,6 @@ make dist
 yum-builddep ovirt-hosted-engine-setup.spec
 rpmbuild \
     -D "_topdir $PWD/tmp.repos" \
-    -D "release_suffix ${SUFFIX}" \
     -ta ovirt-hosted-engine-setup-*.tar.gz
 
 mv *.tar.gz exported-artifacts
