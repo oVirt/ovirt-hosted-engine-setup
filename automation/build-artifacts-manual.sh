a0aeaff7f1b48f601c3dbe4530427c8eb64fd4b4
Sharon Naftaly <snaftaly@redhat.com>
2016-08-17 18:38:51 +0300

    Adding build-artifacts-manual.sh script
    Adding build-artifacts-manual.sh script and related
    packages and repos files.
    build-artifacts-manual.sh is used for creating rpms from an
    existing tarball. It assumes that the tarball already exists
    in the projects directory and builds srpms and rpms, and
    moves the files to exported_artifacts directory.
    
    Change-Id: I4bc32b9ef1ec2679f9beb493c3338abeaad00525
    Signed-off-by: Sharon Naftaly <snaftaly@redhat.com>

---
+++ automation/build-artifacts-manual.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -xe
+[[ -d exported-artifacts ]] \
+|| mkdir -p exported-artifacts
+
+[[ -d tmp.repos ]] \
+|| mkdir -p tmp.repos
+
+SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
+
+autoreconf -ivf
+./configure
+yum-builddep ovirt-hosted-engine-setup.spec
+# Run rpmbuild, assuming the tarball is in the project's directory
+rpmbuild \
+    -D "_topdir $PWD/tmp.repos" \
+    -D "release_suffix ${SUFFIX}" \
+    -ta ovirt-hosted-engine-setup-*.tar.gz
+
+mv *.tar.gz exported-artifacts
+find \
+    "$PWD/tmp.repos" \
+    -iname \*.rpm \
+    -exec mv {} exported-artifacts/ \;
