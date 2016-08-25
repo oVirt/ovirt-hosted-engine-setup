4638eae367c9ef0bf6417fa14b9b77b5559a0a9b
Simone Tiraboschi <stirabos@redhat.com>
2016-08-25 18:38:18 +0200

    upgrade: enforcing current aaa mechanism
    Enforce that all the available aaa domains are
    using the new ovirt-engine-extension mechanism
    since engine 4.0 supports only that one.
    
    Change-Id: I7c64d5a45a67d6a222e65d3fd8fb183d33f5e8ba
    Bug-Url: https://bugzilla.redhat.com/1368604
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>
    (cherry picked from commit a4cbb100feaf4ca4f288777af80e7a8ec8cb4553)

--- src/plugins/gr-he-upgradeappliance/vm/configurevm.py
+++ src/plugins/gr-he-upgradeappliance/vm/configurevm.py
@@ -23,17 +23,23 @@ VM configuration plugin.
 """
 
 
+import configparser
 import gettext
 import hashlib
+import lzma
 import os
+import shutil
 import tarfile
+import tempfile
 
+from io import StringIO
 
 from otopi import plugin
 from otopi import util
 
 
 from ovirt_hosted_engine_setup import constants as ohostedcons
+from ovirt_hosted_engine_setup import engineapi
 
 
 def _(m):
@@ -48,6 +54,101 @@ class Plugin(plugin.PluginBase):
 
     def __init__(self, context):
         super(Plugin, self).__init__(context=context)
+        self._tmp_files_directory_name = None
+
+    def _validate_authz(self, files_tar):
+        self.logger.info(_("Validating authentication plugins"))
+        authz_ext = set([])
+        flist = files_tar.getmembers()
+        self.logger.debug('Content:')
+        self.logger.debug([f.name for f in flist])
+        authplist = [
+            f for f in flist if f.isfile() and
+            'etc/ovirt-engine/extensions.d' in f.name and
+            f.name.endswith('.properties')
+        ]
+        self.logger.debug('Configured plugins:')
+        self.logger.debug([ap.name for ap in authplist])
+        for authp in authplist:
+            authp_file = files_tar.extractfile(authp)
+            auth_f_str = '[section]\n' + authp_file.read()
+            auth_fp = StringIO(unicode(auth_f_str))
+            config = configparser.RawConfigParser()
+            try:
+                config.readfp(auth_fp)
+            except configparser.Error as ex:
+                msg = _(
+                    'The extension configuration file \'{authp}\' inside '
+                    'the backup seams invalid, '
+                    'please check its content on the engine VM and fix: {ex}'
+                ).format(
+                    authp=authp,
+                    ex=ex
+                )
+                self.logger.error(msg)
+                return False
+            if (
+                config.has_section('section') and
+                config.has_option(
+                    'section',
+                    'ovirt.engine.extension.provides'
+                ) and
+                config.has_option(
+                    'section',
+                    'ovirt.engine.extension.name'
+                )
+            ):
+                provides = config.get(
+                    'section',
+                    'ovirt.engine.extension.provides'
+                )
+                name = config.get(
+                    'section',
+                    'ovirt.engine.extension.name'
+                )
+                self.logger.debug(
+                    'Extension {n} provides {p}'.format(
+                        n=name,
+                        p=provides
+                    )
+                )
+                if provides == 'org.ovirt.engine.api.extensions.aaa.Authz':
+                    authz_ext.add(name)
+            else:
+                msg = _(
+                    'The extension configuration file \'{authp}\' inside '
+                    'the backup seams invalid, '
+                    'please check its content on the engine VM and fix.'
+                ).format(
+                    authp=authp,
+                )
+                self.logger.error(msg)
+                return False
+        self.logger.debug(
+            'Authz extensions configured on fs: {l}'.format(l=authz_ext)
+        )
+        engine_api = engineapi.get_engine_api(self)
+        eng_authz_domains = set([
+            d.get_name() for d in engine_api.domains.list()
+        ])
+        self.logger.debug(
+            'Authz domains configured on the engine: {l}'.format(
+                l=eng_authz_domains
+            )
+        )
+        if eng_authz_domains > authz_ext:
+            to_be_fixed = eng_authz_domains - authz_ext
+            msg = _(
+                '{tbf}: such AAA domains are still configured in a '
+                'deprecated way that is not compatible with the current '
+                'release; please upgrade them to ovirt-engine-extension '
+                'mechanism before proceeding.'
+            ).format(
+                tbf=[d for d in to_be_fixed],
+            )
+            self.logger.error(msg)
+            raise RuntimeError('Unsupported AAA mechanism')
+        return True
 
     def _validate_backup_file(self, backup_file_path):
         self.logger.info(
@@ -129,6 +230,64 @@ class Plugin(plugin.PluginBase):
                 )
                 tar.close()
                 return False
+
+        self._tmp_files_directory_name = tempfile.mkdtemp()
+
+        tar.extract(
+            tar.getmember('./files'),
+            path=self._tmp_files_directory_name
+        )
+
+        # tarfile on Python 2 doesn't natively support xz compression
+        # which is the engine-backup default
+        try:
+            uncompressed_file = lzma.LZMAFile(
+                os.path.join(
+                    self._tmp_files_directory_name,
+                    './files'
+                )
+            )
+            try:
+                files_tar = tarfile.open(
+                    fileobj=uncompressed_file,
+                    mode='r:'
+                )
+            except tarfile.ReadError as ex:
+                self.logger.error(
+                    _("'{path}' is not a valid archive: {m}").format(
+                        path='./files',
+                        m=ex.message,
+                    )
+                )
+                tar.close()
+                return False
+        except lzma.error:
+            self.logger.debug('Not lzma')
+            try:
+                files_tar = tarfile.open(
+                    fileobj=tar.extractfile(tar.getmember('./files')),
+                    mode='r:*'
+                )
+            except tarfile.ReadError as ex:
+                self.logger.error(
+                    _(
+                        "'{path}' is not a valid archive: {m} - please try "
+                        "recreating the backup with "
+                        "'--files-compressor=gzip' option."
+                    ).format(
+                        path='./files',
+                        m=ex.message,
+                    )
+                )
+                tar.close()
+                return False
+
+        auth_valid = self._validate_authz(files_tar)
+        files_tar.close()
+        if not auth_valid:
+            tar.close()
+            return False
+
         self.logger.info(
             _("'{backup_file_path}' is a sane backup file").format(
                 backup_file_path=backup_file_path
@@ -188,13 +347,14 @@ class Plugin(plugin.PluginBase):
     def _customization(self):
         valid = False
         interactive = self.environment[ohostedcons.Upgrade.BACKUP_FILE] is None
+        backup_file_path = self.environment[ohostedcons.Upgrade.BACKUP_FILE]
         while not valid:
             # TODO: ensure that the engine is still at 3.6!!!
             # TODO: do it automatically
             self.dialog.note(_(
                 'Please take a backup of the current engine running this '
                 'command on the engine VM:\n'
-                ' engine-backup --mode=backup '
+                ' engine-backup --mode=backup --archive-compressor=gzip '
                 '--file=engine_backup.tar.gz --log=engine_backup.log\n'
                 'Then copy the backup archive to this host and input here '
                 'its path when ready.\n'
@@ -216,5 +376,12 @@ class Plugin(plugin.PluginBase):
                 ] = backup_file_path
             if not valid and not interactive:
                 raise RuntimeError(_('Invalid backup file'))
+
+    @plugin.event(
+        stage=plugin.Stages.STAGE_CLEANUP,
+    )
+    def _cleanup(self):
+        if self._tmp_files_directory_name is not None:
+            shutil.rmtree(self._tmp_files_directory_name)
 
 # vim: expandtab tabstop=4 shiftwidth=4
