79ed472b51d2dbc1e876383ad8e90c204647e436
Simone Tiraboschi <stirabos@redhat.com>
2016-11-21 15:01:26 +0100

    upgrade: fixing engine backup location if not /root
    On upgrades, the engine backup file is always injected
    under /root on the appliance.
    Fixing if the user keeps on different location on the host.
    Using otopi's resolveFile to make it safe against
    relative paths.
    Making the destination path user configurable.
    
    Change-Id: I864154043a545b8f4c3fa069c9ddfaef19280b34
    Bug-Url: https://bugzilla.redhat.com/1394740
    Bug-Url: https://bugzilla.redhat.com/1396193
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- src/plugins/gr-he-common/vm/boot_disk.py
+++ src/plugins/gr-he-common/vm/boot_disk.py
@@ -164,7 +164,7 @@ class ImageTransaction(transaction.TransactionElement):
         self._parent.logger.debug('disk mounted')
         g.upload(
             self._backup_src,
-            os.path.join('/root/', os.path.basename(self._backup_src))
+            self._parent.environment[ohostedcons.Upgrade.DST_BACKUP_FILE]
         )
         self._parent.logger.debug('backup file uploaded')
         g.umount('/')
@@ -476,6 +476,10 @@ class Plugin(plugin.PluginBase):
         )
         self.environment.setdefault(
             ohostedcons.Upgrade.BACKUP_FILE,
+            None
+        )
+        self.environment.setdefault(
+            ohostedcons.Upgrade.DST_BACKUP_FILE,
             None
         )
         self.environment.setdefault(
