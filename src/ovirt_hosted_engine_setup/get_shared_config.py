8f09e3dd9732314d5525025c16a029220eed1f81
Jenny Tokar <jtokar@redhat.com>
2017-02-15 12:21:24 +0200

    Format errors when getting and setting configuration
    This patch formats and clarifies the errors and gets rid
    of the stack trace when setting or getting configuration
    on shared storage.
    
    Change-Id: Ibbf1a7ac02f43644750b5820648e6ec13137c96a
    Bug-Url: https://bugzilla.redhat.com/1417583
    Signed-off-by: Jenny Tokar <jtokar@redhat.com>

--- src/ovirt_hosted_engine_setup/get_shared_config.py
+++ src/ovirt_hosted_engine_setup/get_shared_config.py
@@ -44,6 +44,28 @@ class GetSharedConfig(object):
                 _('Cannot connect to the HA daemon, please check the logs.\n')
             )
             return None
+        except KeyError:
+            config_keys_for_type = ha_cli.get_all_config_keys(config_type)
+            sys.stderr.write(
+                _('Invalid configuration key {key}.\n'.
+                  format(key=key)
+                 )
+            )
+            sys.stderr.write(
+                _('Available keys are:\n')
+            )
+            for c_type in config_keys_for_type:
+                sys.stderr.write(
+                    _('{c_type} : {keys}\n'.
+                      format(c_type=c_type,
+                             keys=config_keys_for_type[c_type]
+                            )
+                     )
+                )
+            return None
+        except Exception as e:
+            sys.stderr.write(str(e) + '\n')
+            return None
 
 if __name__ == "__main__":
     get_shared_config = GetSharedConfig()
