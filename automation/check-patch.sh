244bfa30b335b05120612594509d040d21783163
Yedidyah Bar David <didi@redhat.com>
2018-01-11 15:53:51 +0200

    2_ovirt_logger.py: Log more data
    - Log all (most, at least) vars each time they change
    - Allow filtering sensitive stuff
    - Test filtering in automation
    
    There are probably still missing things. One I noticed is that if you
    run e.g. the command module, and do not register the result, we do not
    log rc. If you do not set 'failed_when: false', it will fail, so you'll
    notice.
    
    Change-Id: I3de36305b98e8e0c69176a64b573d0a80641e24d
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,7 +1,28 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 autoreconf -ivf
 ./configure
 make test
 make distcheck
 
 ./automation/build-artifacts.sh
+
+export PREFIX=$(mktemp -d /tmp/he-prefix.XXXXXX)
+make install prefix="${PREFIX}"
+
+mkdir -p exported-artifacts/tests
+
+export HE_ANSIBLE_LOG_PATH=exported-artifacts/tests/filter-test-$(date -u +%Y%m%d%H%M%S).log
+export ANSIBLE_STDOUT_CALLBACK=2_ovirt_logger
+export ANSIBLE_CALLBACK_PLUGINS=src/ansible
+export PYTHONPATH="${PREFIX}/lib/python2.7/site-packages"
+
+ANSIBLE_LOG=exported-artifacts/tests/filter-test-ansible-output-$(date -u +%Y%m%d%H%M%S).log
+
+# TODO remove this test once we build otopi for python3
+if python -c 'from otopi import util' > /dev/null 2>&1; then
+	ansible-playbook --inventory=localhost, -vvvvvv tests/ansible/playbooks/test_filtering.yml > "${ANSIBLE_LOG}" 2>&1
+	if grep 'secret_data' $HE_ANSIBLE_LOG_PATH; then
+		echo Found non-filtered secrets in the log ^^^
+		exit 1
+	fi
+fi
--- src/ansible/callback_plugins/2_ovirt_logger.py
+++ src/ansible/callback_plugins/2_ovirt_logger.py
@@ -23,14 +23,18 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import json
 import logging
 import os
+import pprint
+import re
 
 
+from collections import defaultdict
 from datetime import datetime
 
 from ansible.plugins.callback import CallbackBase
+
+from dateutil import tz
 
 __metaclass__ = type
 
@@ -45,11 +49,35 @@ DOCUMENTATION = '''
     requirements:
       - whitelisting in configuration
     options:
-      server:
+      log file:
         description: log file
         env:
-          - name: ANSIBLE_LOG_PATH
+          - name: HE_ANSIBLE_LOG_PATH
         default: None
+      filtered tokens var:
+        description:
+          - The name of a variable, that contains a list of tokes
+          - These tokens are filtered out in the log
+        env:
+          - name: HE_ANSIBLE_LOG_FILTERED_TOKENS_VAR
+        default: he_filtered_tokens
+      filtered regular expressions var:
+        description: >
+          The name of a variable, that contains a list of regular expressions.
+          The expressions must each include a group called 'filter'.
+          Filter out content of the group 'filter', on matches.
+          Example: 'a(?P<filter>.*)b' will cause 'axyzb' to be replaced
+          with: 'a**FILTERED**b'.
+        env:
+          - name: HE_ANSIBLE_LOG_FILTERED_TOKENS_RE_VAR
+        default: he_filtered_tokens_re
+      filtered variables var:
+        description:
+          - The name of a variable, that contains a list of variable names
+          - The content of these variables is filtered out in the log
+        env:
+          - name: HE_ANSIBLE_LOG_FILTERED_TOKENS_VARS_VAR
+        default: he_filtered_tokens_vars
 '''
 
 
@@ -57,7 +85,7 @@ class CallbackModule(CallbackBase):
     """
     ansible ovirt_logger callback plugin
     This plugin makes use of the following environment variables:
-        LOGSTASH_SERVER   (mandatory): defaults to None
+        HE_ANSIBLE_LOG_PATH   (mandatory): defaults to None
     """
 
     CALLBACK_VERSION = 2.0
@@ -65,16 +93,252 @@ class CallbackModule(CallbackBase):
     CALLBACK_NAME = 'ovirt_logger'
     CALLBACK_NEEDS_WHITELIST = True
 
+    # Copied from otopi:src/plugins/otopi/core/log.py
+    # and changed a bit
+    class _MyFormatter(logging.Formatter):
+        """Filter strings from log entries."""
+
+        def _get_re_objects(self, regexps):
+            for exp in regexps:
+                if exp not in self._re_objects:
+                    obj = re.compile(exp)
+                    if 'filter' in obj.groupindex:
+                        self._re_objects[exp] = obj
+                    else:
+                        # TODO consider log that we ignored an RE because
+                        # it has no group 'filter', but not sure this is
+                        # a good idea (log from inside a formatter).
+                        pass
+            return self._re_objects.values()
+
+        def _filter(self, content, tokens, regexps):
+            """
+            Filter overlapping tokens within content.
+            regexps is a list of regular expressions, each having a group named
+            'filter'. The content of this group will be filtered.
+
+            Examples:
+            content=abcabca, tokens=('abca')
+            content=aaabbbccc, tokens=('bbb', 'abbba')
+            content=aaaababbbb, tokens=('aaab', 'aaa', 'bbb')
+            content=a BS some secret ES b, regexps=('BS (?P<filter>.*) ES')
+            """
+
+            def _insertFilter(content, begin, end):
+                return content[:begin] + '**FILTERED**' + content[end:]
+
+            tofilter = []
+
+            for token in tokens:
+                if token not in (None, ''):
+                    index = -1
+                    while True:
+                        index = content.find(token, index+1)
+                        if index == -1:
+                            break
+                        tofilter.append((index, index + len(token)))
+
+            for reobj in self._get_re_objects(regexps):
+                if reobj is not None:
+                    index = -1
+                    while True:
+                        matchobj = reobj.search(content, index)
+                        if matchobj is None:
+                            break
+                        index = matchobj.start('filter')
+                        tofilter.append((index, matchobj.end('filter')))
+
+            tofilter = sorted(tofilter, key=lambda e: e[1], reverse=True)
+            begin = None
+            end = None
+            for entry in tofilter:
+                if begin is None or entry[1] < begin:
+                    if begin is not None:
+                        content = _insertFilter(content, begin, end)
+                    begin = entry[0]
+                    end = entry[1]
+                elif entry[0] < begin:
+                    begin = entry[0]
+            else:
+                if begin is not None:
+                    content = _insertFilter(content, begin, end)
+
+            return content
+
+        def __init__(
+            self,
+            fmt=None,
+            datefmt=None,
+            vars_cache=None,
+            filtered_tokens_var=None,
+            filtered_tokens_re_var=None,
+            filtered_vars_var=None,
+        ):
+            logging.Formatter.__init__(self, fmt=fmt, datefmt=datefmt)
+            self._vars_cache = vars_cache
+            self._filtered_tokens_var = filtered_tokens_var
+            self._filtered_tokens_re_var = filtered_tokens_re_var
+            self._filtered_vars_var = filtered_vars_var
+            self._re_objects = {}
+
+        def converter(self, timestamp):
+            return datetime.fromtimestamp(
+                timestamp,
+                tz.tzlocal()
+            )
+
+        def formatTime(self, record, datefmt=None):
+            ct = self.converter(record.created)
+            if datefmt:
+                s = ct.strftime(datefmt, ct)
+            else:
+                s = "%s,%03d%s" % (
+                    ct.strftime('%Y-%m-%d %H:%M:%S'),
+                    record.msecs,
+                    ct.strftime('%z')
+                )
+            return s
+
+        def _get_filtered_tokens(self):
+            res = []
+            for host, hostvars in self._vars_cache.items():
+                res.extend(hostvars.get(self._filtered_tokens_var, []))
+                res.extend([
+                    hostvars.get(k, None)
+                    for k in
+                    hostvars.get(self._filtered_vars_var, [])
+                ])
+            return res
+
+        def _get_filtered_regexps(self):
+            res = []
+            for hostvars in self._vars_cache.values():
+                res.extend(
+                    hostvars.get(self._filtered_tokens_re_var, [])
+                )
+            return res
+
+        def format(self, record):
+            return self._filter(
+                content=logging.Formatter.format(self, record),
+                tokens=self._get_filtered_tokens(),
+                regexps=self._get_filtered_regexps()
+            )
+
+    _logger = None
+    _handler = None
+
+    def _setup_logging(self):
+        if CallbackModule._logger is None:
+            # ansible instantiates callbacks twice in load_callbacks.
+            # Setup logging only once, or we get too much logging...
+            CallbackModule._handler = logging.StreamHandler(self.handle)
+            CallbackModule._handler.setLevel(logging.DEBUG)
+            CallbackModule._handler.setFormatter(self._MyFormatter(
+                fmt='%(asctime)s %(levelname)s %(message)s',
+                vars_cache=self._vars_cache,
+                filtered_tokens_var=self._filtered_tokens_var,
+                filtered_tokens_re_var=self._filtered_tokens_re_var,
+                filtered_vars_var=self._filtered_tokens_vars_var,
+            ))
+            CallbackModule._logger = logging.getLogger(
+                "ovirt-hosted-engine-setup-ansible"
+            )
+            CallbackModule._logger.addHandler(CallbackModule._handler)
+            CallbackModule._logger.setLevel(logging.DEBUG)
+        self.logger = CallbackModule._logger
+
+    def _collect_vars_changes(self, hostname, newvars):
+        res = []
+        for k, v in newvars.items():
+            if (
+                hostname not in self._vars_cache or
+                k not in self._vars_cache[hostname] or
+                str(v) != str(self._vars_cache[hostname][k]) or
+                not isinstance(v, type(self._vars_cache[hostname][k]))
+            ):
+                res.append(
+                    'var changed: host "{h}" var "{k}" type "{t}" '
+                    'value: "{v}"'.format(
+                        h=hostname,
+                        k=k,
+                        t=type(v),
+                        v=str(v),
+                    )
+                )
+        return res
+
+    def _update_vars_cache(self):
+        vars_changes = []
+        if self.varmgr:
+            for host in self.varmgr._inventory.get_hosts():
+                vars_changes.extend(
+                    self._collect_vars_changes(
+                        str(host),
+                        self.varmgr.get_vars(host=host)
+                    )
+                )
+                self._vars_cache[str(host)].update(
+                    self.varmgr.get_vars(host=host)
+                )
+                if self.play:
+                    vars_changes.extend(
+                        self._collect_vars_changes(
+                            str(host),
+                            self.varmgr.get_vars(play=self.play, host=host)
+                        )
+                    )
+                    self._vars_cache[str(host)].update(
+                        self.varmgr.get_vars(play=self.play, host=host)
+                    )
+            # Log changes after applying them, so that our filter handles
+            # changes before we log them.
+            for line in vars_changes:
+                self.logger.debug(line)
+
+    def dump_obj(self, obj):
+        name = repr(obj)[:100]
+        return '\n'.join([
+            'type: %s' % type(obj),
+            'str: %s' % str(obj)[:300],
+            'repr: %s' % repr(obj)[:300],
+            'dir: %s' % dir(obj),
+            'pprint: %s' % pprint.pformat(obj)[:300],
+        ] + [
+            '%s.%s: %s' % (
+                name,
+                attr,
+                pprint.pformat(getattr(obj, attr))
+            )
+            for attr in dir(obj)
+            if not callable(getattr(obj, attr))
+        ])
+
     def __init__(self):
         super(CallbackModule, self).__init__()
 
-        logFileName = os.getenv('ANSIBLE_LOG_PATH', None)
+        logFileName = os.getenv('HE_ANSIBLE_LOG_PATH', None)
+        self._filtered_tokens_var = os.getenv(
+            'HE_ANSIBLE_LOG_FILTERED_TOKENS_VAR',
+            'he_filtered_tokens'
+        )
+        self._filtered_tokens_re_var = os.getenv(
+            'HE_ANSIBLE_LOG_FILTERED_TOKENS_RE_VAR',
+            'he_filtered_tokens_re'
+        )
+        self._filtered_tokens_vars_var = os.getenv(
+            'HE_ANSIBLE_LOG_FILTERED_TOKENS_VARS_VAR',
+            'he_filtered_tokens_vars'
+        )
         self.playbook = None
+        self.play = None
+        self.varmgr = None
+        self._vars_cache = defaultdict(dict)
 
         if not logFileName:
             self.disabled = True
             self._display.warning(
-                "No log file specified with ANSIBLE_LOG_PATH"
+                "No log file specified with HE_ANSIBLE_LOG_PATH"
             )
         else:
             try:
@@ -89,29 +353,20 @@ class CallbackModule(CallbackBase):
                     mode='a',
                     buffering=1,
                 )
+            self._setup_logging()
 
-            self._handler = logging.StreamHandler(self.handle)
-            self._handler.setLevel(logging.DEBUG)
-            self._handler.setFormatter(logging.Formatter(
-                '%(asctime)s %(levelname)s %(name)s '
-                '%(module)s.%(funcName)s:%(lineno)d '
-                '%(message)s'
-            ))
-            self.logger = logging.getLogger(
-                "ovirt-hosted-engine-setup-ansible"
-            )
-            self.logger.addHandler(self._handler)
         self.start_time = datetime.utcnow()
         self.errors = 0
 
     def v2_playbook_on_start(self, playbook):
-        self.playbook = playbook._file_name
+        self.playbook = playbook
         data = {
             'status': "OK",
             'ansible_type': "start",
-            'ansible_playbook': self.playbook,
+            'ansible_playbook': self.playbook._file_name,
         }
-        self.logger.info("ansible start", extra=data)
+        self.logger.info("ansible start playbook %s", self.playbook._file_name)
+        self.logger.debug("ansible start %s" % data)
 
     def v2_playbook_on_stats(self, stats):
         end_time = datetime.utcnow()
@@ -130,22 +385,27 @@ class CallbackModule(CallbackBase):
             'ansible_type': "finish",
             'ansible_playbook': self.playbook,
             'ansible_playbook_duration': runtime.total_seconds(),
-            'ansible_result': json.dumps(summarize_stat),
+            'ansible_result': self.dump_obj(summarize_stat),
         }
-        self.logger.info("ansible stats", extra=data)
+        self.logger.info("ansible stats %s " % data)
 
     def v2_runner_on_ok(self, result, **kwargs):
+        self._update_vars_cache()
         data = {
             'status': "OK",
             'ansible_type': "task",
             'ansible_playbook': self.playbook,
             'ansible_host': result._host.name,
             'ansible_task': result._task,
-            'ansible_result': self._dump_results(result._result)
         }
-        self.logger.info("ansible ok", extra=data)
+        self.logger.info("ansible ok %s " % data)
+        self.logger.info(
+            "ansible ok result._result %s " %
+            self.dump_obj(result._result)
+        )
 
     def v2_runner_on_skipped(self, result, **kwargs):
+        self._update_vars_cache()
         data = {
             'status': "SKIPPED",
             'ansible_type': "task",
@@ -153,9 +413,10 @@ class CallbackModule(CallbackBase):
             'ansible_task': result._task,
             'ansible_host': result._host.name
         }
-        self.logger.info("ansible skipped", extra=data)
+        self.logger.info("ansible skipped %s" % data)
 
     def v2_playbook_on_import_for_host(self, result, imported_file):
+        self._update_vars_cache()
         data = {
             'status': "IMPORTED",
             'ansible_type': "import",
@@ -163,9 +424,10 @@ class CallbackModule(CallbackBase):
             'ansible_host': result._host.name,
             'imported_file': imported_file
         }
-        self.logger.info("ansible import", extra=data)
+        self.logger.info("ansible import %s" % data)
 
     def v2_playbook_on_not_import_for_host(self, result, missing_file):
+        self._update_vars_cache()
         data = {
             'status': "NOT IMPORTED",
             'ansible_type': "import",
@@ -173,39 +435,58 @@ class CallbackModule(CallbackBase):
             'ansible_host': result._host.name,
             'missing_file': missing_file
         }
-        self.logger.info("ansible import", extra=data)
+        self.logger.info("ansible import %s" % data)
 
     def v2_runner_on_failed(self, result, **kwargs):
+        self._update_vars_cache()
         data = {
             'status': "FAILED",
             'ansible_type': "task",
             'ansible_playbook': self.playbook,
             'ansible_host': result._host.name,
             'ansible_task': result._task,
-            'ansible_result': self._dump_results(result._result)
+            'ansible_result': self.dump_obj(result._result)
         }
         self.errors += 1
-        self.logger.error("ansible failed", extra=data)
+        self.logger.error("ansible failed %s" % data)
 
     def v2_runner_on_unreachable(self, result, **kwargs):
+        self._update_vars_cache()
         data = {
             'status': "UNREACHABLE",
             'ansible_type': "task",
             'ansible_playbook': self.playbook,
             'ansible_host': result._host.name,
             'ansible_task': result._task,
-            'ansible_result': self._dump_results(result._result)
+            'ansible_result': self.dump_obj(result._result)
         }
-        self.logger.error("ansible unreachable", extra=data)
+        self.logger.error("ansible unreachable %s" % data)
 
     def v2_runner_on_async_failed(self, result, **kwargs):
+        self._update_vars_cache()
         data = {
             'status': "FAILED",
             'ansible_type': "task",
             'ansible_playbook': self.playbook,
             'ansible_host': result._host.name,
             'ansible_task': result._task,
-            'ansible_result': self._dump_results(result._result)
+            'ansible_result': self.dump_obj(result._result)
         }
         self.errors += 1
-        self.logger.error("ansible async", extra=data)
+        self.logger.error("ansible async %s" % data)
+
+    def v2_playbook_on_play_start(self, play):
+        self.play = play
+        self.varmgr = self.play.get_variable_manager()
+        data = {
+            'status': "OK",
+            'ansible_type': "play start",
+            'ansible_play': self.play.name,
+        }
+        self.logger.info("ansible play start %s" % data)
+
+    def v2_on_any(self, *args, **kwargs):
+        self.logger.debug("ansible on_any args %s kwargs %s" % (args, kwargs))
+
+
+# vim: expandtab tabstop=4 shiftwidth=4
