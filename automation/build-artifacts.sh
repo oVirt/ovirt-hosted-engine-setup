b918f50ae141cdd2c996a40c2e903bc297e6872e
Simone Tiraboschi <stirabos@redhat.com>
2018-10-24 17:40:17 +0200

    build: post ovirt-hosted-engine-setup-2.2.30
    Change-Id: Iafd8be6b0d0945c7fd75b7f36abce245ba99d78b
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -5,6 +5,8 @@
 [[ -d tmp.repos ]] \
 || mkdir -p tmp.repos
 
+SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
+
 autoreconf -ivf
 ./configure
 make dist
@@ -17,6 +19,7 @@ fi
 
 rpmbuild \
     -D "_topdir $PWD/tmp.repos" \
+    -D "release_suffix ${SUFFIX}" \
     -ta ovirt-hosted-engine-setup-*.tar.gz
 
 mv *.tar.gz exported-artifacts
