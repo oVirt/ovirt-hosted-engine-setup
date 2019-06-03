ff598e98f480cc97a0ca59ef453295f42dc4a148
Sandro Bonazzola <sbonazzo@redhat.com>
2019-06-03 15:02:05 +0200

    automation: skip distcheck on broken automake
    Added workaround for http://bugzilla.redhat.com/1716384
    skipping make distcheck on broken automake.
    
    Change-Id: I190c1e1204936a7b72287dd403b79c4606775657
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-merged.sh
+++ automation/check-merged.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -e
 autoreconf -ivf
 ./configure
+if [ `rpm -qv automake` != automake-1.16.1-5.fc29.noarch ]
+then
+# Workaround for http://bugzilla.redhat.com/1716384
 make distcheck
+fi
--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -3,7 +3,12 @@
 autoreconf -ivf
 ./configure
 make test
+if [ `rpm -qv automake` != automake-1.16.1-5.fc29.noarch ]
+then
+# Workaround for http://bugzilla.redhat.com/1716384
 make distcheck
+fi
+exit
 
 ./automation/build-artifacts.sh
 
