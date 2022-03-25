deaec4877ec90ea23be8beb3317c530fb0e7f258
Sandro Bonazzola <sbonazzo@redhat.com>
2022-03-25 09:29:49 +0100

    pep8: fix reported issues
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- src/ansible/callback_plugins/1_otopi_json.py
+++ src/ansible/callback_plugins/1_otopi_json.py
@@ -30,7 +30,7 @@ import json
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))  # noqa: E402
 from constants import AnsibleCallback
 from constants import Const as ansiblecons
 
